c4ec51ed6d04a4599b723d4f8078a6e6242be68a
aloise <aloise@aloise.name>
2012-08-03 13:10:07 +0300

    Application homepage

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import java.util.Set;
+
+import models.MusicCategory;
+
 import play.mvc.*;
 
 import views.html.*;
@@ -8,9 +12,24 @@ public class ApplicationController extends Controller {
   
   public static Result index() 
   {
-    
-	  return ok( Application_index.render());
-    
+	  return category(0);
+  }
+  
+  public static Result category(Integer parentCategoryId)
+  {
+	  MusicCategory parentCategory = parentCategoryId <= 0 ? null : MusicCategory.find.where().eq("id", parentCategoryId).findUnique();
+	  
+	  int parentCategoriesCount = 4;
+	  Set<MusicCategory> categories = MusicCategory.find.where().eq("parent_id", parentCategoryId).setMaxRows(parentCategoriesCount).findSet();
+	  
+	  return ok( Application_index.render(parentCategory, categories));	  
+  }
+  
+  public static Result playlistByCategory(Integer categoryId)
+  {
+	  MusicCategory category = MusicCategory.find.where().eq("id", categoryId).findUnique();
+	  
+	  return ok( Application_playlistByCategory.render(category) );
   }
   
 }
