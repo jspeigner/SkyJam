4d7083af88c5e79ef9be72ca941aa34efde4d1d0
Aloise <aloise@aloise.name>
2012-10-28 21:36:04 +0200

    [+] admin - browse users

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -2,12 +2,16 @@ package controllers;
 
 import java.util.Date;
 
+import com.avaje.ebean.Page;
+
+import models.Playlist;
 import models.User;
 import models.UserRole;
 import controllers.UserController.Login;
 import be.objectify.deadbolt.actions.Restrict;
 import play.mvc.Result;
 import play.data.Form;
+import play.db.ebean.Model.Finder;
 
 public class AdminController extends BaseController {
 
@@ -61,6 +65,15 @@ public class AdminController extends BaseController {
         	
 
         }
+    }
+    
+    @Restrict(UserRole.ROLE_ADMIN)
+    public static Result browseUsers(Integer page, String term ){
+    	
+    	int pageSize = 15;
+    	Page<User> users = User.getPageWithSearch(page, pageSize, term);
+    	
+    	return ok(views.html.Admin.browseUsers.render(users, term));
     }
 
 	
