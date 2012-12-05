d420e20fb6936d1666e3d059eea39deb0b8a3274
Aloise <aloise@aloise.name>
2012-12-05 23:36:54 +0200

    [+] search updates

--- app/controllers/SongController.java
+++ app/controllers/SongController.java
@@ -22,7 +22,7 @@ public class SongController extends BaseController {
 		  }
 		  else
 		  {
-			  List<Song> songs = Song.searchWideByName( query.trim() , 100);
+			  List<Song> songs = Song.searchWideByName( query , 100);
 			  
 			  return ok(views.html.Song.searchJson.render(songs)).as( Global.JSON_CONTENT_TYPE );
 		  }
